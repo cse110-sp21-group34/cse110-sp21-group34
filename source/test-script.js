7af1f0b896161fe321aabefe5386b75543e962de
Kalidasan Dheeraj <marslandersltd@gmail.com>
2021-05-18 09:14:13 +0530

    Fix style on test-script.js

--- source/test-script.js
+++ source/test-script.js
@@ -4,13 +4,12 @@
  * @param  {string} text - The text to be outputted
  * @return {string} - The text that is outputted
  */
-        function generateText(text) {
+function generateText(text) {
   return text;
 }
 
 document.getElementById("test_button").addEventListener("click", () => {
   const newElem = document.createElement("p");
-  let i = 5;
   newElem.textContent = generateText("Welcome!");
   newElem.id = "test_content";
   document.body.appendChild(newElem);
