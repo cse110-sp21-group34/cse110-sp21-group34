d821ed5473a5f29365ba1bcc4de31e0c754892f2
Kalidasan Dheeraj <marslandersltd@gmail.com>
2021-05-18 11:33:11 +0530

    Fixed dependencies

--- source/editor_v2/index.js
+++ source/editor_v2/index.js
@@ -1,115 +1,120 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+/* eslint-disable no-new */
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 // const Delimiter = require('@editorjs/delimiter');
-import DragDrop from 'editorjs-drag-drop';
-import Undo from 'editorjs-undo';
-
+import DragDrop from "editorjs-drag-drop";
+import Undo from "editorjs-undo";
 
 const editor = new EditorJS({
-
-  holderId: 'editorjs',
+  holderId: "editorjs",
 
   onReady: () => {
-      new Undo({ editor });
-      new DragDrop(editor);
-    },
+    new Undo({ editor });
+    new DragDrop(editor);
+  },
   tools: {
-      list: {
-        class: NestedList,
-        inlineToolbar: true,
-      },
+    list: {
+      class: NestedList,
+      inlineToolbar: true,
+    },
 
-      paragraph: {
-        class: Paragraph,
-        inlineToolbar: true,
-        tunes: ['anyTuneName'],
-      },
+    paragraph: {
+      class: Paragraph,
+      inlineToolbar: true,
+      tunes: ["anyTuneName"],
+    },
 
-      header: {
-        class: Header,
-        inlineToolbar: true,
-        shortcut: 'CMD+SHIFT+H',
-        tunes: ['anyTuneName'],
-      },
-      
-      checklist: {
-          class: Checklist,
-          inlineToolbar: true,
-      },
+    header: {
+      class: Header,
+      inlineToolbar: true,
+      shortcut: "CMD+SHIFT+H",
+      tunes: ["anyTuneName"],
+    },
 
-      warning: {
-          class: Warning,
-          inlineToolbar: true,
-          shortcut: 'CMD+SHIFT+W',
-          config: {
-            titlePlaceholder: 'Title',
-            messagePlaceholder: 'Message',
-          },
-        },
+    checklist: {
+      class: Checklist,
+      inlineToolbar: true,
+    },
 
-      quote: {
-        class: Quote,
-        inlineToolbar: true,
-        shortcut: 'CMD+SHIFT+O',
-        config: {
-          quotePlaceholder: 'Enter a quote',
-          captionPlaceholder: 'Quote\'s author',
-        },
-      },
-
-      AnyButton: {
-        class: AnyButton,
-        inlineToolbar: false,
-        config:{
-          css:{
-            "btnColor": "btn--gray",
-          }
-        }
-      },
-
-      Marker: {
-        class: Marker,
-        shortcut: 'CMD+SHIFT+M',
-      },
-
-      anyTuneName: {
-        class:AlignmentBlockTune,
-        config:{
-          default: "right",
-          blocks: {
-            header: 'center',
-            list: 'right'
-          }
-        },
-      },
-
-      image: {
-        class: ImageTool,
-        config: {
-          endpoints: {
-            byFile: 'http://localhost:8000/uploadFile', // Your backend file uploader endpoint
-            byUrl: 'http://localhost:8000/fetchUrl', // Your endpoint that provides uploading by Url
-          }
-        }
-      },
-
-      embed: {
-        class: Embed,
-        config: {
-          services: {
-            youtube: true,
-            coub: true
-          }
-        }
+    warning: {
+      class: Warning,
+      inlineToolbar: true,
+      shortcut: "CMD+SHIFT+W",
+      config: {
+        titlePlaceholder: "Title",
+        messagePlaceholder: "Message",
       },
     },
+
+    quote: {
+      class: Quote,
+      inlineToolbar: true,
+      shortcut: "CMD+SHIFT+O",
+      config: {
+        quotePlaceholder: "Enter a quote",
+        captionPlaceholder: "Quote's author",
+      },
+    },
+
+    AnyButton: {
+      class: AnyButton,
+      inlineToolbar: false,
+      config: {
+        css: {
+          btnColor: "btn--gray",
+        },
+      },
+    },
+
+    Marker: {
+      class: Marker,
+      shortcut: "CMD+SHIFT+M",
+    },
+
+    anyTuneName: {
+      class: AlignmentBlockTune,
+      config: {
+        default: "right",
+        blocks: {
+          header: "center",
+          list: "right",
+        },
+      },
+    },
+
+    image: {
+      class: ImageTool,
+      config: {
+        endpoints: {
+          byFile: "http://localhost:8000/uploadFile", // Your backend file uploader endpoint
+          byUrl: "http://localhost:8000/fetchUrl", // Your endpoint that provides uploading by Url
+        },
+      },
+    },
+
+    embed: {
+      class: Embed,
+      config: {
+        services: {
+          youtube: true,
+          coub: true,
+        },
+      },
+    },
+  },
 });
 
-let saveBtn = document.querySelector('button');
+const saveBtn = document.querySelector("button");
 
-saveBtn.addEventListener('click', ()=>{
-  editor.save().then((outputData) => {
-    console.log('Article data: ', outputData);
-    localStorage.setItem('Your content', outputData);
-  }).catch((error) => {
-    console.log('Saving failed: ', error)
-  });
+saveBtn.addEventListener("click", () => {
+  editor
+    .save()
+    .then((outputData) => {
+      console.log("Article data: ", outputData);
+      localStorage.setItem("Your content", outputData);
+    })
+    .catch((error) => {
+      console.log("Saving failed: ", error);
+    });
 });
