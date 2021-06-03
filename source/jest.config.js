45821c47d33246b43a4f11d3fde52aebb93bc356
Yang Yue <yayue@ucsd.edu>
2021-06-02 21:12:27 -0700

    Add dependencies

--- source/jest.config.js
+++ source/jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  "verbose": true,
-  "silent": true,
-  "preset": "jest-puppeteer"
+ // "verbose": true,
+ // "silent": true,
+ // "preset": "jest-puppeteer",
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  }
+
 }
