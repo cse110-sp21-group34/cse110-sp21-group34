3b3f45d9a9aa3e6fc83116ef2222a77ef326eba4
gordon-zhao <gordon-zhao@users.noreply.github.com>
2021-06-06 02:46:06 +0800

    test proposal

--- source/jest-puppeteer.config.js
+++ source/jest-puppeteer.config.js
@@ -1,6 +1,4 @@
-// Do not need this file now since we are not doing e2e testing.
-
-/*module.exports = {
+module.exports = {
     launch: {
        headless: false,
        slowMo: 500
@@ -10,4 +8,4 @@
         command: 'parcel editing-page.html -p 8000',
         port: 8000
     }
-}*/
+}
