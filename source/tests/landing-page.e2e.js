b633c14f5d0cc1248d3e2f53d03eed348feb8afb
gordon-zhao <gordon-zhao@users.noreply.github.com>
2021-06-06 19:26:52 +0800

    Fixing landing page e2e

--- source/tests/landing-page.e2e.js
+++ source/tests/landing-page.e2e.js
@@ -3,26 +3,25 @@ const { Keyboard } = require("puppeteer");
 describe('Basic user flow for SPA ', () => {
     beforeAll(async () => {
       await page.goto('http://localhost:8000');
-      await page.waitForTimeout(500);
-    });
+    }, 30000);
 
-    it('Test1', async () => {
+    it('Correct number of months', async () => {
       const numMonths = await page.$$("option[class='oneMonth']");
       expect(numMonths.length).toBe(12);
     });
 
-    it('Test2', async () => {
+    it('Text input', async () => {
       const input = await page.$("div[id='editor']");
       await input.click();
 
-      await page.keyboard.press('m');
-      await page.keyboard.press('e');
-      await page.keyboard.press('s');
-      await page.keyboard.press('s');
-      await page.keyboard.press('a');
-      await page.keyboard.press('g');
-      await page.keyboard.press('e');
-      
+      await page.waitForTimeout(1000); // Wait for element to come up
+
+      await page.keyboard.type('message');
+
+      const content = await page.evaluate(() => {
+        return document.getElementsByClassName('cdx-nested-list__item-content')[0].textContent;
+      })
+      expect(content).toBe('message');
       /*const localStorageData = await page.evaluate(() => {
         let json = {};
         for (let i = 0; i < localStorage.length; i++) {
@@ -32,10 +31,10 @@ describe('Basic user flow for SPA ', () => {
         }
         return json;
       });*/
-      const localStorageData = await page.evaluate(() =>  Object.assign({}, window.localStorage));
+      // const localStorageData = await page.evaluate(() =>  Object.assign({}, window.localStorage));
 
-      console.log(localStorageData);
-      expect(localStorageData).toBe("message");
+      // console.log(localStorageData);
+      // expect(localStorageData).toBe("message");
     }, 15000);
 
 })
