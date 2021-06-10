18b24a86ff2a78e06bff3ac10b6ed64998e156b3
Zhuoliang Pu <zhpu@ucsd.edu>
2021-06-10 18:14:54 +0800

    Revert "Revert "Add temp testing""
    This reverts commit 1ed4f8168b39b0056f7e76d960476e44dcab133d.

--- source/editor/tests/editor.jsdom.js
+++ source/editor/tests/editor.jsdom.js
@@ -38,4 +38,45 @@ describe("Editor", () => {
         expect(newJournal.settings).toEqual({});
         expect(storage.assets).toEqual({})
     });
+
+
+    it('Testing assets with dexiesWrapper', async() => {
+        const {Assets, AssetsDexieWrapper} = require('../../storage/assets');
+        var aFileParts = ['<a id="a"><b id="b">hey!</b></a>']; // an array consisting of a single DOMString
+        var blob= new Blob(aFileParts, {type : 'text/html'}); // the blob
+
+        const newAssetDB = new AssetsDexieWrapper('test-assets')
+        const newAsset = new Assets(newAssetDB);
+        const uid = newAsset.save(blob);
+        newAssetDB.submit(blob);
+
+        console.log = jest.fn()
+        newAssetDB.retrieve(uid);
+        expect(newAsset.get(uid)).resolves.toEqual(blob);
+        newAssetDB.remove(uid);
+        expect(newAsset.del(uid)).toBe(0);
+        
+    });
+
+
+    
+    it('Testing assets with mockWrapper', async() => {
+        const {Assets, AssetsMockWrapper} = require('../../storage/assets');
+        var aFileParts = ['<a id="a"><b id="b">hey!</b></a>']; // an array consisting of a single DOMString
+        var blob= new Blob(aFileParts, {type : 'text/html'}); // the blob
+
+        const newAssetDB = new AssetsMockWrapper('test-assets')
+        const newAsset = new Assets(newAssetDB);
+        const uid = newAsset.save(blob);
+        newAssetDB.submit(blob);
+
+        
+        console.log = jest.fn()
+        newAssetDB.retrieve(uid);
+        expect(newAsset.get(uid)).resolves.toEqual(blob);
+        newAssetDB.remove(uid);
+        expect(newAsset.del(uid)).toBe(0);
+        
+        
+    });
 });
